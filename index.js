8417a011e119d9639bec020c8b52a17663ce38ac
MikChanna <channa.mik@gmail.com>
2020-07-26 15:45:28 -0400

    writes to md file

--- index.js
+++ index.js
@@ -49,43 +49,26 @@ function promptUser() {
   ]);
 }
 
-promptUser();
+function generateHTML(answers) {
+  return `
+  # ${answers.title}
+  ## ${answers.description}
+  ## ${answers.usage}
+  ## ${answers.installation}
+  ## ${answers.credits}
+  ## ${answers.license}
+  `;
+}
 
-// function generateHTML(answers) {
-//   return `
-//   <!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//     <title>Document</title>
-//   </head>
-//   <body>
-//     <div class="jumbotron jumbotron-fluid">
-//     <div class="container">
-//       <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-//       <p class="lead">I am from ${answers.location}.</p>
-//       <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//       <ul class="list-group">
-//         <li class="list-group-item">My GitHub username is ${answers.github}</li>
-//         <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-//       </ul>
-//     </div>
-//   </div>
-//   </body>
-//   </html>`;
-// }
+promptUser()
+  .then(function (answers) {
+    const markdown = generateHTML(answers);
 
-// promptUser()
-//   .then(function (answers) {
-//     const html = generateHTML(answers);
-
-//     return writeFileAsync("index.html", html);
-//   })
-//   .then(function () {
-//     console.log("Sucvessfully wrote to index.html");
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
+    return writeFileAsync("index.md", markdown);
+  })
+  .then(function () {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
